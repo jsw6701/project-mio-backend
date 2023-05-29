f4b376e933d239e38f28f252a6b54c27bf12d37e
jsw6701 <anes53027@gmail.com>
2023-05-29 16:07:26 +0900

    Refactor: 배포 properties 추가

--- src/main/java/com/gdsc/projectmiobackend/dto/request/CategoryPatchRequestDto.java
+++ src/main/java/com/gdsc/projectmiobackend/dto/request/CategoryPatchRequestDto.java
@@ -1,8 +1,10 @@
 package com.gdsc.projectmiobackend.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CategoryPatchRequestDto {
 
     private Long categoryId;
