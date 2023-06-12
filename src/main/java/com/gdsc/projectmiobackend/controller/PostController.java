d3924739114f8c7ef05806c141d2004efa4c37ca
jsw6701 <anes53027@gmail.com>
2023-06-12 22:17:16 +0900

    HOTFIX : 게시글 Null 값 입력되는 오류

--- src/main/java/com/gdsc/projectmiobackend/controller/PostController.java
+++ src/main/java/com/gdsc/projectmiobackend/controller/PostController.java
@@ -20,10 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.util.UUID;
 
 
 @AllArgsConstructor
