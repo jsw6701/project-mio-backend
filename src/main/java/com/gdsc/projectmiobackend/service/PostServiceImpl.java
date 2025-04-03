adc818e2b05c2a30433c718c88606282ec989c93
Glenn <anes53027@gmail.com>
2025-04-03 23:52:49 +0900

    fix : 알림 비동기 처리

--- src/main/java/com/gdsc/projectmiobackend/service/PostServiceImpl.java
+++ src/main/java/com/gdsc/projectmiobackend/service/PostServiceImpl.java
@@ -167,7 +167,7 @@ public class PostServiceImpl implements PostService{
     }
 
     /**
-     * SSE 알림과 DB 알림 저장 비동기처리(test)
+     * SSE 알림과 DB 알림 저장 비동기처리
      *
      * @param user
      * @param post
